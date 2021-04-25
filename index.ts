a4085e5f73128ef9c201fe2c4c82f0b9daef55ef
Henry Catalini Smith <smith@hen.cat>
2021-04-25 20:47:45 +0200

    more verbose

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 import { Logger } from "eazy-logger"
 import fs from "fs"
 import rehype from "rehype"
-import parse from "rehype-parse"
-import stringify from "rehype-stringify"
 import unified from "unified/types/ts4.0/index"
+import { name, version } from "./package.json"
 
 interface EleventyConfig {
   addTransform: (
@@ -16,28 +15,29 @@ interface Options {
   plugins?: unified.Plugin<[], rehype.RehypeOptions>[]
 }
 
-const pkg = JSON.parse(fs.readFileSync(`${__dirname}/package.json`, "utf-8"))
-
 export const rehypePlugin = {
   initArguments: {},
   configFunction: function(eleventyConfig: EleventyConfig, options) {
     const logger = Logger({
-      prefix: `[{blue:${pkg.name}}] `,
+      prefix: `[{blue:${name}}@{blue:${version}}] `,
     })
 
     const processor = rehype()
-    processor.use(parse)
     ;(options.plugins || []).forEach(plugin => {
       processor.use.apply(processor, plugin)
     })
-    processor.use(stringify)
 
     eleventyConfig.addTransform(
-      pkg.name,
+      name,
       function(content: string, outputPath: string): string {
         if (outputPath && outputPath.endsWith(".html")) {
+
+          const start = process.hrtime()
           content = processor.processSync(content).toString()
-          logger.info(`transformed {green:${outputPath}}`)
+          const end = process.hrtime(start)
+          const time = Math.ceil(end[0] * 1e9 + end[1] / 1e6)
+
+          logger.info(`transformed {green:${outputPath}} [{magenta:${time}ms}]`)
         }
         return content
       }
